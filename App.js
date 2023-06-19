5a2f20f54081fd0faca522141bfae949313db1e1
ohnas <ohnas12@gmail.com>
2023-06-19 14:37:48 +0900

    App.js

--- App.js
+++ App.js
@@ -1,21 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import * as SplashScreen from 'expo-splash-screen';
+import Root from './navigation/Root';
+import { DBContext } from "./context";
+
+// Keep the splash screen visible while we fetch resources
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+  const { RealmProvider } = DBContext;
+  const [appIsReady, setAppIsReady] = useState(false);
+  async function prepare() {
+    try {
+      // 추후에, 로딩이 필요한 요소들 집어넣기(이미지 등)
+    } catch (e) {
+      console.warn(e);
+    } finally {
+      // Tell the application to render
+      setAppIsReady(true);
+      await SplashScreen.hideAsync();
+    }
+  };  
+  useEffect(() => {
+    prepare();
+  }, [])
+
+  if (!appIsReady) {
+    return null;
+  }
+  
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <RealmProvider>
+      <NavigationContainer>
+          <Root />
+          <StatusBar style="dark" />
+      </NavigationContainer>
+    </RealmProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
